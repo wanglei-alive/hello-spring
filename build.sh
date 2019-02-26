0800f09b125ce1186758072e83ae0038631ffefd
wanglei150 <wanglei29@jd.com>
2019-02-26 23:16:41 +0800

    build.sh

---
+++ build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -eu
+mvn --version
+Prompt(){
+	echo "[$(date '+%D %H:%M:%S')] $@"
+}
+
+WORKSPACE=$(cd $(dirname $0); pwd)
+Prompt "Work dir: $WORKSPACE"
+
+cd $WORKSPACE
+
+export LANG="en_US.UTF-8"
+
+Prompt "Setting up environment variables OK..."
+
+
+OUTPUT=$WORKSPACE/target/demo-1.0-SNAPSHOT-all/output
+
+Prompt "Clean and Package"
+mvn clean -s settings.xml -U package -Dmaven.test.skip=true -Dhttps.protocols=TLSv1,TLSv1.1,TLSv1.2 || exit $?
+Prompt "Package for deploy"
+rm -fr $WORKSPACE/output
+mv $OUTPUT ./
+
+mkdir $WORKSPACE/output/opbin
+
+Prompt "Generate version and timestamp..."
+echo $(date -d  today +%Y%m%d%H%M%S) > $WORKSPACE/output/version
+git log | head -1 | awk '{print $2}' >> $WORKSPACE/output/version
+Prompt "version::"`cat $WORKSPACE/output/version`
+
+Prompt "download jdk"
+curl http://code-deploy-plugin.oss.cn-north-1.jcloudcs.com/jdk-8u111-linux-x64.tar.gz > $WORKSPACE/output/jdk-8u111-linux-x64.tar.gz
+
+Prompt "build finish exit:"$?
+
+exit $?
